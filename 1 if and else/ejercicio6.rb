18caa58ce0f598985c6a9a9a63a93a5d827aed68
mycamach <manoli.camacho.a@gmail.com>
2017-08-21 20:36:22 -0300

    6 ready

--- 1 if and else/ejercicio6.rb
+++ 1 if and else/ejercicio6.rb
@@ -1,15 +1,9 @@
-# Utiliza álgebra booleana para hacer un refactoring de este código
+# Utiliza algebra booleana para hacer un refactoring de este codigo
 # Para verificar la respuestas, puedes modificar los valores de a y b.
 
 a = 'verdadero'
 b = 'falso'
 
-if a == 'verdadero'
-    puts ':)'
-else
-  if b == 'verdadero'
-    puts ':|'
-  else
-    puts ':('
-  end
-end
+puts ':)' if a == 'verdadero'
+puts ':|' if b == 'verdadero'
+puts ':(' if a == 'verdadero' && b == 'falso'
