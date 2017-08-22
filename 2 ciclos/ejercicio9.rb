42543a61c29a7e43a8cacda33ba1e33e6e8bba22
mycamach <manoli.camacho.a@gmail.com>
2017-08-22 16:42:15 -0300

    Added Rubocop changes to 9

--- 2 ciclos/ejercicio9.rb
+++ 2 ciclos/ejercicio9.rb
@@ -18,18 +18,17 @@ b = ['<table>']
 
 b.push('	<tbody>')
 
-b.push('			<tr>')
+b.push('		<tr>')
 
 3.times do
-	i+=1
-	b.push("				<td> #{i} </td>")
+  i += 1
+  b.push("			<td> #{i} </td>")
 end
 
-b.push('			</tr>')
+b.push('		</tr>')
 
 b.push('	</tbody>')
 
 b.push('</table>')
-
 
 puts b
