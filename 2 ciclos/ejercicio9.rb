5ef7e0223c2ec18294c5416de827db17aacc783b
mycamach <manoli.camacho.a@gmail.com>
2017-08-22 16:36:06 -0300

    9 ready

--- 2 ciclos/ejercicio9.rb
+++ 2 ciclos/ejercicio9.rb
@@ -12,8 +12,24 @@
 
 =end
 
-a = 
+i = 0
+
 b = ['<table>']
 
-a.times do 
-	b.push ['<td>']
+b.push('	<tbody>')
+
+b.push('			<tr>')
+
+3.times do
+	i+=1
+	b.push("				<td> #{i} </td>")
+end
+
+b.push('			</tr>')
+
+b.push('	</tbody>')
+
+b.push('</table>')
+
+
+puts b
