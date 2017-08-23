4cbc1e9e4fb2e22f046378fc2e67f55b9aee8dc7
mycamach <manoli.camacho.a@gmail.com>
2017-08-23 19:12:52 -0300

    3 almost ready, missing 0 exit option

--- 3 ciclos anidados/ejercicio3.rb
+++ 3 ciclos anidados/ejercicio3.rb
@@ -2,3 +2,15 @@
 # la tabla de multiplicar del número ingresado. Debe repetir la operación hasta
 # que se ingrese un 0 (cero).
 # Ingrese un número (0 para salir): _
+
+puts 'Ingrese un número (0 para salir)'
+n = gets.chomp.to_i
+
+i = 1
+
+while i <= 12 && i >= 0
+  resultado = n * i
+  puts "#{n} x #{i} = #{resultado}"
+  i += 1
+  break if i.zero?
+end
